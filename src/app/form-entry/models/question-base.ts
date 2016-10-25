7be0599d4004ac09e6bdaa2c5830b6c5620ea544
Brian Kimutai <kimutaibryan@gmail.com>
2016-10-25 09:42:42 +0300

    OFO-1:Design and implement the question model base.

---
+++ src/app/form-entry/models/question-base.ts
@@ -0,0 +1,21 @@
+import { Validation } from './validation';
+export class QuestionBase<T>{
+    value?: T;
+    type: string;
+    key: string;
+    label?: string;
+    order?: number;
+    emitChanges?: boolean;
+    settings?: {};
+    validation?: Array<Validation>;
+    constructor(options: QuestionBase<any>) {
+        this.value = options.value;
+        this.type = options.type;
+        this.key = options.key || '';
+        this.label = options.label || '';
+        this.order = options.order === undefined ? 1 : options.order;
+        this.emitChanges = options.emitChanges || true;
+        this.validation = options.validation || [];
+    }
+}
+
